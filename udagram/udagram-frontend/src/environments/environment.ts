4a6c2a3f2c22db2b6ee6f907e391c7726dcd2489
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2022-11-03 19:35:24 +0200

    Adds api/v0

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-mgpqrxmf.us-east-1.elasticbeanstalk.com/'
+  apiHost: 'http://udagram-api-dev.eba-mgpqrxmf.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
