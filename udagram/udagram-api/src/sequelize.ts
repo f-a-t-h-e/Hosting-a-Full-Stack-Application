ac1b2d0bd4b4eb564d8fdf5d319d76487fa96854
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2022-11-03 19:54:24 +0200

    add ports

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -6,7 +6,7 @@ export const sequelize = new Sequelize({
   password: config.password,
   database: config.database,
   host: config.host,
-  port: config.DB_PORT
+  port: config.db_port,
   dialect: "postgres",
   storage: ":memory:",
 });
