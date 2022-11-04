ca9d0e8eb00cacc7626dd52460352ba87c0cb476
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2022-11-04 06:09:43 +0200

    hmmmm

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv AWS_BUCKET=arn:aws:s3:::$AWS_BUCKET
+eb setenv AWS_REGION=$AWS_REGION
+eb setenv AWS_PROFILE=default
+eb setenv JWT_SECRET=$JWT_SECRET
+eb setenv URL=$http://udagram-api.eba-shimm7t6.us-east-1.elasticbeanstalk.com/
